290a313e5ad662b88d09bb4e5fb335eb6d3c2ef9
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-05-13 11:51:44 +0300

    Update SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+USE [ADO.NET_30.04]
+GO
+
+INSERT INTO StudentAssessments (StudentName, GroupName, AverageScore, MinSubject, MaxSubject) 
+VALUES ('John Doe', 'Group A', 85.5, 'Mathematics', 'History');
